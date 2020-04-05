dc5dcbc8bdf2eeb78f44f45360e65bcb0b9ac5c4
li <leejw2012@163.com>
2020-04-05 20:41:14 +0800

    初始化Vue项目

--- src/main.js
+++ src/main.js
@@ -2,6 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// 全部导入手动配置element-ui
+// import ElementUI from 'element-ui'
+// import 'element-ui/lib/theme-chalk/index.css'
+// Vue.use(ElementUI)
+
+/* 导入全局样式表 */
+import './assets/css/global.css'
+
+import './plugins/element.js'
+
 Vue.config.productionTip = false
 
 new Vue({
