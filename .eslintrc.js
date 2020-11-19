3f21ea6727772ded51f43f9b204ec1d73565ddd2
lijiawen <leejw2012@163.com>
2020-11-19 16:50:18 +0800

    修改配制

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,30 @@
-// module.exports = {
-//   root: true,
-//   env: {
-//     node: true
-//   },
-//   extends: [
-//     'plugin:vue/essential',
-//     '@vue/standard'
-//   ],
-//   parserOptions: {
-//     parser: 'babel-eslint'
-//   },
-//   rules: {
-//     // 'indent': ['error', 4],
-//     'quotes': [
-//         'error',
-//         'double'
-//     ],
-//     'semi': [
-//         'error',
-//         'always'
-//     ],
-//     'space-before-function-paren': [
-//         'error',
-//         'never'
-//     ],
-//     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-//     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
-//   }
-// }
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: [
+    'plugin:vue/essential',
+    '@vue/standard'
+  ],
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  rules: {
+    // 'indent': ['error', 4],
+    // 'quotes': [
+    //     'error',
+    //     'double'
+    // ],
+    // 'semi': [
+    //     'error',
+    //     'always'
+    // ],
+    // 'space-before-function-paren': [
+    //     'error',
+    //     'never'
+    // ],
+    // 'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    // 'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+  }
+}
